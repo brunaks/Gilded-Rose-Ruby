adfba6bfc84a903f46cf2e01ac9c6b486452df73
bruna <brunak_s@hotmail.com>
2020-02-26 14:53:08 -0300

    empty message

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -12,6 +12,10 @@ class UpdaterFactory
 
     if item.name == 'Aged Brie'
       AgedBrieUpdater.new(item)
+
+    elsif item.name == 'Sulfuras, Hand of Ragnaros'
+      SulfurasUpdater.new(item)
+
     else
       DefaultUpdater.new(item)
     end
@@ -21,6 +25,56 @@ class UpdaterFactory
 end
 
 class DefaultUpdater
+
+  def initialize item
+    @item = item
+  end
+
+  def update
+
+    item = @item
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      if item.quality > 0
+        if item.name != 'Sulfuras, Hand of Ragnaros'
+          item.quality -= 1
+        end
+      end
+    else
+      if item.quality < 50
+        item.quality += 1
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality += 1
+            end
+          end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality += 1
+            end
+          end
+        end
+      end
+    end
+    if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in -= 1
+    end
+    if item.sell_in < 0
+      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality > 0
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            item.quality -= 1
+          end
+        end
+      else
+        item.quality = item.quality - item.quality
+      end
+    end
+  end
+
+end
+
+class SulfurasUpdater
 
   def initialize item
     @item = item

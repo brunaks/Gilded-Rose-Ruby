9d54ad45b46a074a2f8985f66ff89d56343e249b
bruna <brunak_s@hotmail.com>
2020-02-26 15:26:20 -0300

    empty message

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -62,9 +62,7 @@ class DefaultUpdater
     item.sell_in -= 1
 
     if item.sell_in < 0
-      if item.quality > 0
-        item.quality -= 1
-      end
+      @quality_updater.update(-1)
     end
   end
 
